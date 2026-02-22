008a696258fb2f818d274f10f8e2722dfbaabbc9
Daniel Nakhooda <dnakhooda@gmail.com>
2026-02-22 13:08:14 -0500

    ner tool chain

--- Core/Src/stm32h5xx_hal_msp.c
+++ Core/Src/stm32h5xx_hal_msp.c
@@ -1070,48 +1070,6 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
 }
 
 /**
-  * @brief TIM_Base MSP De-Initialization
-  * This function freeze the hardware resources used in this example
-  * @param htim_base: TIM_Base handle pointer
-  * @retval None
-  */
-void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
-{
-  if(htim_base->Instance==TIM1)
-  {
-    /* USER CODE BEGIN TIM1_MspDeInit 0 */
-
-    /* USER CODE END TIM1_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_TIM1_CLK_DISABLE();
-
-    /**TIM1 GPIO Configuration
-    PE14     ------> TIM1_CH4
-    PD5     ------> TIM1_CH4N
-    */
-    HAL_GPIO_DeInit(GPIOE, GPIO_PIN_14);
-
-    HAL_GPIO_DeInit(GPIOD, GPIO_PIN_5);
-
-    /* USER CODE BEGIN TIM1_MspDeInit 1 */
-
-    /* USER CODE END TIM1_MspDeInit 1 */
-  }
-  else if(htim_base->Instance==TIM3)
-  {
-    /* USER CODE BEGIN TIM3_MspDeInit 0 */
-
-    /* USER CODE END TIM3_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_TIM3_CLK_DISABLE();
-    /* USER CODE BEGIN TIM3_MspDeInit 1 */
-
-    /* USER CODE END TIM3_MspDeInit 1 */
-  }
-
-}
-
-/**
   * @brief TIM_IC MSP De-Initialization
   * This function freeze the hardware resources used in this example
   * @param htim_ic: TIM_IC handle pointer
