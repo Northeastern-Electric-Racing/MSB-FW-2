4b0b52464e1808e9a61686cb843eac5e07b73142
Aolgeirson <asher@olgeirson.com>
2026-02-22 16:13:31 -0500

    CAN ID changes

--- Core/Inc/u_can.h
+++ Core/Inc/u_can.h
@@ -2,11 +2,11 @@
 
 #include "u_tx_can.h"
 
-#define SHT30_CAN_ID 0xBAA
-#define IMU_ACCEL_CAN_ID 0xBAB
-#define IMU_GYRO_CAN_ID 0xBAC
-#define MAGNOMETER_CAN_ID 0xBAD
-#define ORIENTATION_CAN_ID 0xBAE
-#define VL53L7CX_CAN_ID 0xBAF
+#define SHT30_CAN_ID 0xBAA       // 0xBBE for rear
+#define IMU_ACCEL_CAN_ID 0xBAB   // 0xBBF for rear
+#define IMU_GYRO_CAN_ID 0xBAC    // 0xBC0 for rear
+#define MAGNOMETER_CAN_ID 0xBAD  // 0xBC1 for rear
+#define ORIENTATION_CAN_ID 0xBAE // 0xBC2 for rear
+#define VL53L7CX_CAN_ID 0xBAF    // 0xBC3 for rear, measures ride height
 
 // u_TODO - finish this file
