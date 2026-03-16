cafb05d3b26ebfa36960503e01cac62f248933e2
Daniel Nakhooda <dnakhooda@gmail.com>
2026-03-15 20:48:42 -0400

    start

--- Core/Inc/u_wheel_speed.h
+++ Core/Inc/u_wheel_speed.h
@@ -3,21 +3,20 @@
 
 #include "stm32h5xx_hal.h"
 
-// Number of magnets on each wheel
-#define MAGNETS_PER_REV  12
+// Number of pulses per wheel rotation
+#define PULSES_PER_ROTATION  12
 
 // Timer clock after prescaler (set this to match CubeMX)
 // e.g. 200MHz / prescaler of 200 = 1MHz
 #define TIM_CLOCK_HZ     1000000
 
-void WheelSpeed_Init(TIM_HandleTypeDef *htim_fl, TIM_HandleTypeDef *htim_fr,
-                     FDCAN_HandleTypeDef *hcan);
+void wheel_speed_init(TIM_HandleTypeDef *htim_left, TIM_HandleTypeDef *htim_right);
 
 // Call from HAL_TIM_IC_CaptureCallback
-void WheelSpeed_CaptureCallback(TIM_HandleTypeDef *htim);
+void wheel_speed_capture_callback(TIM_HandleTypeDef *htim);
 
 // Call at your desired publish rate (e.g. 250Hz task)
-void WheelSpeed_PublishCAN(void);
+void send_wheel_speed();
 
 extern float fl_rpm;
 extern float fr_rpm;
