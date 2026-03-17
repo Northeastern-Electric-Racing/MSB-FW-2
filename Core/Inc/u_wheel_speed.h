5d26a79b084f0ec1207162872bbccc8e5e4f3fe4
Daniel Nakhooda <dnakhooda@gmail.com>
2026-03-16 20:35:28 -0400

    fix

--- Core/Inc/u_wheel_speed.h
+++ Core/Inc/u_wheel_speed.h
@@ -4,7 +4,7 @@
 #include "stm32h5xx_hal.h"
 
 /** Number of pulses per wheel rotation */
-#define PULSES_PER_ROTATION 4
+#define PULSES_PER_ROTATION 8
 
 /** HAL Effect timers speed */
 #define TIM_CLOCK_HZ        1000000
