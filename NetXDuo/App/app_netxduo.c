6adf1a2602b4ee3c74e92459b1f47820a43c1a99
Daniel Nakhooda <dnakhooda@gmail.com>
2026-04-05 21:39:30 -0400

    fix

--- NetXDuo/App/app_netxduo.c
+++ NetXDuo/App/app_netxduo.c
@@ -2,7 +2,18 @@
 /**
   ******************************************************************************
   * @file    app_netxduo.c
+  * @author  MCD Application Team
   * @brief   NetXDuo applicative file
+  ******************************************************************************
+    * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
   ******************************************************************************
   */
 /* USER CODE END Header */
@@ -12,10 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "u_nx_ethernet.h"
-#include "u_threads.h"
 #include "u_ethernet.h"
-#include "u_board_config.h" // Include the board configuration header for board-specific definitions
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -25,16 +33,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-/* Define board-specific name and ID */
-#ifdef MSB_BOARD_1
-    #define BOARD_NAME "MSB Board 1"
-    #define BOARD_ID 1
-#elif defined(MSB_BOARD_2)
-    #define BOARD_NAME "MSB Board 2"
-    #define BOARD_ID 2
-#else
-    #error "No MSB board defined. Please define MSB_BOARD_1 or MSB_BOARD_2."
-#endif
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -63,29 +62,9 @@ UINT MX_NetXDuo_Init(VOID *memory_ptr)
   TX_BYTE_POOL *byte_pool = (TX_BYTE_POOL*)memory_ptr;
 
    /* USER CODE BEGIN App_NetXDuo_MEM_POOL */
-    (void)byte_pool;
+  (void)byte_pool;
   /* USER CODE END App_NetXDuo_MEM_POOL */
   /* USER CODE BEGIN 0 */
-    int status;
-
-    // Log the board name and ID
-    PRINTLN_INFO("Initializing Ethernet for %s (ID: %d)...", BOARD_NAME, BOARD_ID);
-
-    // Initialize Ethernet
-    status = ethernet1_init();
-    if (status != U_SUCCESS) {
-        PRINTLN_ERROR("Ethernet initialization failed for %s (ID: %d) (Status: %d)", BOARD_NAME, BOARD_ID, status);
-        return NX_NOT_SUCCESSFUL;
-    }
-    PRINTLN_INFO("Ethernet initialized successfully for %s (ID: %d).", BOARD_NAME, BOARD_ID);
-
-    // Initialize threads
-    status = threads_init(byte_pool);
-    if (status != U_SUCCESS) {
-        PRINTLN_ERROR("Thread initialization failed for %s (ID: %d) (Status: %d)", BOARD_NAME, BOARD_ID, status);
-        return NX_NOT_SUCCESSFUL;
-    }
-    PRINTLN_INFO("Threads initialized successfully for %s (ID: %d).", BOARD_NAME, BOARD_ID);
   /* USER CODE END 0 */
 
   /* USER CODE BEGIN MX_NetXDuo_Init */
